afaaf280b1cb513878c4c993a830b295ec5cf5b0
raghureddie <raghureddie@example.com>
2025-10-29 10:17:49 -0400

    style: fix PEP8 spacing (E302) in service/routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -77,6 +79,8 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_account(account_id):
     """
@@ -85,15 +89,20 @@ def get_account(account_id):
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
 
-    account = Account.find(account_id) if hasattr(Account, "find") else Account.query.get(account_id)
+    account = Account.find(account_id) if hasattr(
+        Account, "find") else Account.query.get(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -102,9 +111,12 @@ def update_accounts(account_id):
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
 
-    account = Account.find(account_id) if hasattr(Account, "find") else Account.query.get(account_id)
+    account = Account.find(account_id) if hasattr(
+        Account, "find") else Account.query.get(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.")
 
     check_content_type("application/json")
 
@@ -123,11 +135,14 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
 
-    account = Account.find(account_id) if hasattr(Account, "find") else Account.query.get(account_id)
+    account = Account.find(account_id) if hasattr(
+        Account, "find") else Account.query.get(account_id)
     if account:
         account.delete()
 
